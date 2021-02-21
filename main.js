0f1c79dc3618e56c2024166714e0cb0ab22b8752
Super Promiser <superpromiser@gmail.com>
2021-02-21 14:27:53 +0800

    updated

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ class BasicWorldDemo {
         this._Initialize();
     }
     _Initialize() {
-        console.log('initialize')
         this._threejs = new THREE.WebGLRenderer();
         this._threejs.shadowMap.enabled = true;
         this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -151,7 +150,6 @@ class BasicWorldDemo {
     }
 
     _Step(timeElapsed) {
-        console.log(timeElapsed)
         const timeElapsedS = timeElapsed * 0.001;
         if (this._mixers) {
             this._mixers.map(m => m.update(timeElapsedS));
@@ -163,80 +161,8 @@ class BasicWorldDemo {
     }
 }
 
-class Website3dDemo {
-    constructor() {
-        this._Initialize();
-    }
-    _Initialize(){
-        this._threejs = new THREE.WebGLRenderer({
-            antialias: true,
-            alpha: true,
-        });
-        this._threejs.shadowMap.enabled = true;
-        this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
-        this._threejs.physicallyCorrectLights = true;
-        this._threejs.toneMapping = THREE.ACESFilmicToneMapping;
-        this._threejs.outputEncoding = THREE.sRGBEncoding;
-        this._threejs.setPixelRatio(window.devicePixelRatio);
-        this._threejs.setSize(window.innerWidth, window.innerHeight);
-
-        document.body.appendChild(this._threejs.domElement);
-
-        window.addEventListener('resize', () => {
-            this._OnWindowResize();
-        },false);
-
-        const fov = 60;
-        const aspect = 1920 / 1080;
-        const near = 1.0;
-        const far = 1000.0;
-        this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
-        this._scene = new THREE.Scene();
-
-        let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-        light.position.set(20, 100, 10);
-        light.target.position.set(0,0,0);
-        light.castShadow = true;
-        light.shadow.bias = -0.001;
-        light.shadow.mapSize.width = 2048;
-        light.shadow.mapSize.height = 2048;
-        light.shadow.camera.near = 0.1;
-        light.shadow.camera.far = 500.0;
-        light.shadow.camera.left = 100;
-        light.shadow.camera.right = -100;
-        light.shadow.camera.top = 100;
-        light.shadow.camera.bottom = -100;
-        this._scene.add(light);
-
-        light = new THREE.AmbientLight(0xFFFFFF);
-        this._scene.add(light);
-
-        const controls = new OrbitControls(this._camera, this._threejs.domElement);
-        controls.target.set(0, 20, 0);
-        controls.update();
-
-        const plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(100,100,10,10),
-            new THREE.MeshStandardMaterial({
-                color:0xFFFFFF
-            })
-        );
-        plane.castShadow = false;
-        plane.receiveShadow = true;
-        plane.rotation.x = -Math.PI / 2;
-        this._scene.add(plane);
-    }
-
-    OnWindowResize() {
-        this._camera.aspect = window.innerWidth / window.innerHeight;
-        this._camera.updateProjectionMatrix();
-        this._threejs.setSize(window.innerWidth, window.innerHeight);
-    }
-}
-
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-  _APP = new Website3dDemo();
+  _APP = new BasicWorldDemo();
 });
